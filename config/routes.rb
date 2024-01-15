709473909d98bb57716dbdc17642d9324f125d92
zeff96 <zeffo1975@gmail.com>
2024-01-15 15:46:43 +0300

    Add initial setup

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+  namespace :auth do
+    post "signup", to: "registrations#create"
+    post "account_confirmation", to: "registrations#account_confirmation"
+    post "login", to: "session#create"
+    post "reset_password", to: "passwords#reset_password"
+    post "edit_password", to: "passwords#update_password"
+  end
+end
