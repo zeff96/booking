83371865e47cce3ef5e9701487b827e7dc86e6de
zeff96 <zeffo1975@gmail.com>
2024-01-27 18:51:33 +0300

    Add method for finding motorcyle with id from params

--- app/controllers/motorcyles_controller.rb
+++ app/controllers/motorcyles_controller.rb
@@ -3,4 +3,12 @@ class MotorcylesController < ApplicationController
     @motorcles = Motorcycle.all
     render json: @motorcles
   end
+
+  
+
+  private
+
+  def set_motorcyle
+    @motorcle = Motorcycle.find(params[:id])
+  end
 end
