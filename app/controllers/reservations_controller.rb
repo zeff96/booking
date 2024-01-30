3a9a2967e80500260bcb3ec2de918e5ed4af5e54
zeff96 <zeffo1975@gmail.com>
2024-01-30 20:00:20 +0300

    Add reservation strong params

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,4 +4,8 @@ class ReservationsController < ApplicationController
   def set_motorcicle
     @user = Motorcircle.find(params[:motorcircle_id])
   end
+
+  def reservation_params
+    params.require(:reservation).permit(:name, :date, :city)
+  end
 end
