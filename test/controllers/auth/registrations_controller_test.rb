f2bb8e9c85b499fc2920aae0d712a30587813b7a
zeff96 <zeffo1975@gmail.com>
2024-02-06 18:06:31 +0300

    Add test for duplicate email

--- test/controllers/auth/registrations_controller_test.rb
+++ test/controllers/auth/registrations_controller_test.rb
@@ -10,4 +10,15 @@ class Auth::RegistrationsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_equal "User signed up successfully", JSON.parse(response.body)["message"]
   end
+
+  test "should not create user with duplicate email" do
+    existing_user = FactoryBot.create(:user)
+
+    assert_no_difference("User.count") do
+      post auth_signup_url, params: { user: { username: "test1", email: existing_user.email, password: "password" } }, as: :json
+    end
+
+    assert_response :unprocessable_entity
+    assert_equal "Email already taken", JSON.parse(response.body)["error"]
+  end
 end
