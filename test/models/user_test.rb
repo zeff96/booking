421689625f11d9011e9e5838b034476ce4ce46dc
zeff96 <zeffo1975@gmail.com>
2024-02-06 08:15:30 +0300

    Add test for user duplicate email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,4 +10,13 @@ class UserTest < ActiveSupport::TestCase
     user = User.new(username: "test")
     assert_not user.save, "Saved without valid email"
   end
+
+  test "should not save user with duplicate email" do
+    user_1 = User.new(username: "test1", email: "test1@gmail.com", password: "password")
+    user_2 = User.new(username: "test2", email: "test1@gmail.com", password: "password")
+
+    user_1.save!
+
+    assert_not user_2.save, "Saved with duplicate email"
+  end
 end
