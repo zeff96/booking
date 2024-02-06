459b408b54bd76f0dbff6f70788f7f39ef51226f
zeff96 <zeffo1975@gmail.com>
2024-02-06 08:08:04 +0300

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :motorcircles, dependent: :destroy
-  validates :username, :email, presence: true
+  validates :username, presence: true
+  validates :email, presence: true, uniqueness: true
 end
