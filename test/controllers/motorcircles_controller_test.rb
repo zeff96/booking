e24c029e6bde011cb41be435756f15e58f9b831e
zeff96 <zeffo1975@gmail.com>
2024-02-07 19:58:32 +0300

    Add functional test for motorcircle creation

--- test/controllers/motorcircles_controller_test.rb
+++ test/controllers/motorcircles_controller_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
-class MotorcylesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class MotorcirclesControllerTest < ActionDispatch::IntegrationTest
+  test "should create motorcircle for logged in user" do
+    user = FactoryBot.create(:user)
+    motorcircle_attributes = FactoryBot.attributes_for(:motorcircle)
+
+    post auth_login_url params: { user: { email: user.email, password: user.password } }
+
+
+    token = JSON.parse(response.body)["token"]
+
+    assert_not_nil token, "Token should not be nil"
+
+    assert_difference("Motorcircle.count") do
+      post motorcircles_url, params: { motorcircle: motorcircle_attributes},
+      headers: { "authorization" => "Bearer #{token}" }
+    end
+
+    assert_response :created
+    assert_equal "created successfully!", JSON.parse(response.body)["message"]
+  end
 end
