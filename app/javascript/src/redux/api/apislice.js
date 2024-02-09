b0972f57ff8cbb32954fc21511e4aba25b3f59a1
zeff96 <zeffo1975@gmail.com>
2024-02-09 23:03:52 +0300

    Add method for setting tokens for every request

--- app/javascript/src/redux/api/apislice.js
+++ app/javascript/src/redux/api/apislice.js
@@ -1,5 +1,19 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const baseQuery = fetchBaseQuery({
+  baseUrl: url,
+  credentials: "include",
+  prepareHeaders(headers, { getState }) {
+    let token = getState().auth.token;
+
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+
+    return headers;
+  },
+});
+
 const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: url }),
   endpoints: () => ({}),
