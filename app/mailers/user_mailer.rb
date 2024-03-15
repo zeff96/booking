34b9516f6889d2f86b18d72eceb9925cbab36c1c
zeff96 <zeffo1975@gmail.com>
2024-03-15 08:41:21 +0300

    Add dynamic url params generation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,14 +3,14 @@ class UserMailer < ApplicationMailer
 
   def confirmation_instructions(user)
     @user = user
-    @url = "http://localhost:3000/auth/account_confirmation?confirmation_token=#{ @user.confirmation_token }"
+    @url = auth_account_confirmation_url(confirmation_token: @user.confirmation_token)
 
     mail(to: @user.email, subject: "Account confirmation")
   end
 
   def reset_password_instructions(user)
     @user = user
-    @url = "http://localhost:3000/auth/edit_password?reset_password_token=#{@user.reset_password_token}"
+    @url = "/edit_password?reset_password_token=#{@user.reset_password_token}"
 
     mail(to: @user.email, subject: "Reset password instructions")
   end
