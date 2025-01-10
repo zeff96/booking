6f5b8d7c0bf2ea498b3eca5e590e50d0521ca09a
zeff96 <zeffo1975@gmail.com>
2025-01-10 13:12:53 +0300

    Update mailer to fetch user from the params

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,17 +1,14 @@
 class UserMailer < ApplicationMailer
   default from: "notifications@example.com"
 
-  def confirmation_instructions(user)
-    @user = user
-    @url = auth_account_confirmation_url(confirmation_token: @user.confirmation_token)
-
-    mail(to: @user.email, subject: "Account confirmation")
+  def email_confirmation
+    @user = params[:user]
+    mail(to: @user.email, subject: "Email Confirmation")
   end
 
-  def reset_password_instructions(user)
-    @user = user
-    @url = "/edit_password?reset_password_token=#{@user.reset_password_token}"
 
+  def reset_password_instructions()
+    @user = params[:user]
     mail(to: @user.email, subject: "Reset password instructions")
   end
 end
