8b3c534656d0cc06878de1ba4454b3cad9eb7c19
zeff96 <zeffo1975@gmail.com>
2025-01-10 13:17:37 +0300

    Update user schema and remove confirmation_token and reset_password_token columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,51 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_29_193011) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_06_154446) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "motorcircles", force: :cascade do |t|
-    t.string "name"
-    t.string "image"
-    t.decimal "finance_fee"
-    t.decimal "option_to_purchase"
-    t.decimal "total_amount_payable"
-    t.integer "duration"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.text "bio"
-    t.index ["user_id"], name: "index_motorcircles_on_user_id"
-  end
-
-  create_table "reservations", force: :cascade do |t|
-    t.string "name"
-    t.date "date"
-    t.string "city"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_reservations_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email", default: "", null: false
-    t.string "password_digest", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.string "confirmation_token"
-    t.datetime "confirmation_sent_at"
+    t.string "username", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "confirmed_at"
     t.string "role", default: "user", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "motorcircles", "users"
-  add_foreign_key "reservations", "users"
 end
