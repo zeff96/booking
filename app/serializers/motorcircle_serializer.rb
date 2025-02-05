2a4a1f9bd0b6ed4ccbffbf209c47fef1361519af
zeff96 <zeffo1975@gmail.com>
2025-02-05 21:12:11 +0300

    Add url for rendering images as remote urls

--- app/serializers/motorcircle_serializer.rb
+++ app/serializers/motorcircle_serializer.rb
@@ -1,10 +1,9 @@
 class MotorcircleSerializer < ActiveModel::Serializer
-  include Rails.application.routes.url_helpers
   attributes :id, :name, :image_url, :bio, :finance_fee, :option_to_purchase, :total_amount_payable, :duration
 
   def image_url
     if object.image.attached?
-      rails_blob_url(object.image, only_path: false)
+      Rails.application.routes.url_helpers.url_for(object.image)
     end
   end
 end
