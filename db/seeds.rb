e46738165da16e6aa46faf57b49055d33c416b55
zeff96 <zeffo1975@gmail.com>
2025-02-10 20:25:59 +0300

    Add random cities data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,47 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#   
+# db/seeds.rb
+
+# cities = [
+#   {
+#     name: "Nairobi",
+#     timezone: "Africa/Nairobi",
+#     booking_open: true,
+#   },
+#   {
+#     name: "Kisumu",
+#     timezone: "Africa/Nairobi",
+#     booking_open: true,
+#   },
+#   {
+#     name: "Mombasa",
+#     timezone: "Africa/Nairobi",
+#     booking_open: true,
+#   },
+#   {
+#     name: "Nakuru",
+#     timezone: "Africa/Nairobi",
+#     booking_open: true,
+#   },
+#   {
+#     name: "Eldoret",
+#     timezone: "Africa/Nairobi",
+#     booking_open: true,
+#   },
+#   {
+#     name: "Kakamega",
+#     timezone: "Africa/Nairobi",
+#     booking_open: true,
+#   }
+# ]
+
+# cities.each do |city_attributes|
+#   City.find_or_create_by!(name: city_attributes[:name]) do |city|
+#     city.timezone = city_attributes[:timezone]
+#     city.booking_open = city_attributes[:booking_open]
+#   end
+# end
+
+
